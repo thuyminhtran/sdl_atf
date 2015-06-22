19f277603adb85fe8d0775d06dc1ad6a153217b6
vveremeva <vveremjova@luxoft.com>
2015-06-22 15:43:51 +0300

    APPLINK-13157 tests for check creation reports and collecting SDL logger

---
+++ test/SDLLogTest.lua
@@ -0,0 +1,75 @@
+xmlLogger = require("logger")
+config    = require("config")
+
+
+local sdl_log = require('sdl_logger')
+local io  = require("atf.stdlib.std.io")
+
+function FindDirectory(directory,currDate)
+    local t, popen = "", io.popen
+    for filename in popen('ls -a "'..directory..'"'):lines() do
+    	if string.find(filename,"SDLLogs_"..currDate,1,true) ~=nil then
+      	  t= filename
+   		end
+    end
+     print("Report folder is :")
+     print("		"..t)
+    return t
+end
+
+
+function  FindReportPath(ReportPath)
+	filereport = assert(io.open(ReportPath,"r"))
+
+	if filereport == nil then 
+		print("ERROR: Directory ".. ReportPath.. " was not found")
+	else 
+		print("Directory ".. ReportPath.. " was successfully found")
+	end
+
+	filereport:close()
+end
+
+function  FindReport(ReportPath, logger_name, currDate)
+
+	local t, popen = "", io.popen
+    for reportName in popen('ls -a "'..ReportPath..'"'):lines() do
+    	if string.find(reportName,logger_name.."_"..currDate,1,true) ~=nil then
+      	  t= reportName
+      	  print("SDL log file :")
+      	  print("		"..reportName.. " was successfully found")    	  
+   		end
+    end
+    if t == "" then
+	    print("ERROR: SDL log file does not exist")
+	end
+    return t
+end
+
+function cleanTestDirectory(ReportPath,ReportName)
+	res, err  = os.remove(ReportName)
+	if not res then
+		print(err)
+	else 
+		print("Test report "..ReportName.. " was successfully deleted")
+	end
+end
+
+
+--=================================
+print ("Sdl Logger test started")
+print("==============\n")
+
+dates = os.date("%Y%m%d%H%M")
+
+ReportPath = FindDirectory("./Testing Reports/",dates)
+ReportPath = "Testing Reports/".. ReportPath.."/test"
+
+FindReportPath(ReportPath)
+
+logger_name = "SDLLogTest"
+ReportName = FindReport(ReportPath,logger_name,dates)
+
+print("============== \n")
+print ("Test ended")
+quit()
---
+++ test/reportTest.lua
@@ -0,0 +1,160 @@
+xmlLogger = require("logger")
+config    = require("config")
+local io  = require("atf.stdlib.std.io")
+function FindDirectory(directory,currDate)
+    local t, popen = "", io.popen
+    for filename in popen('ls -a "'..directory..'"'):lines() do
+    	if string.find(filename,"TestingReports_"..currDate,1,true) ~=nil then
+      	  t= filename
+   		end
+    end
+     print("Report folder is :")
+     print("		"..t)
+    return t
+end
+
+
+function  FindReportPath(ReportPath)
+	filereport = assert(io.open(ReportPath,"r"))
+
+	if filereport == nil then 
+		print("ERROR: Directory ".. ReportPath.. " was not found")
+	else 
+		print("Directory ".. ReportPath.. " was successfully found")
+	end
+
+	filereport:close()
+end
+
+function  FindReport(ReportPath, xmlName, currDate)
+
+	 local t, popen = "", io.popen
+    for reportName in popen('ls -a "'..ReportPath..'"'):lines() do
+    	if string.find(reportName,xmlName.."_"..currDate,1,true) ~=nil then
+      	  t= reportName
+      	  print("Xml report file :")
+      	  print("		"..reportName.. " was successfully found")    	  
+   		end
+    end
+    if t == "" then
+	    print("ERROR: Xml report file does not exist")
+	end
+    return t
+end
+
+function cleanTestDirectory(ReportPath,ReportName)
+	res, err  = os.remove(ReportName)
+	if not res then
+		print(err)
+	else 
+		print("Test report "..ReportName.. " was successfully deleted")
+	end
+
+end
+
+function createXmlFile(xmlname)
+	 xmlLogger.init(tostring("test/"..xmlname))
+	 xmlLogger.AddCase("InitHMI")	
+	 xmlLogger.CaseMessageTotal(xmlLogger.current_case_name,{ ["result"] = "success", ["timestamp"] ="100"} )
+	 xmlLogger.AddMessage("EXPECT_HMIEVENT", {["FunctionName"] = "Connected websocket"})
+	 xmlLogger.AddMessage("hmi_connection", {["FunctionName"] = "SendRequest"},"{[\"methodName\"] = \"New method\"}")
+	 xmlLogger:finalize()
+end
+
+function AnalyzeXmlReport(ReportPath,ReportName)
+	filereport = assert(io.open(ReportPath.."/"..ReportName,"r"))
+
+    line = filereport:read()  
+    if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end  	
+	line = filereport:read()  
+	if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end  
+	if line ~= "<test_validationTest_lua>" then 
+		print("ERROR: incorrect case, get "..line)
+	end
+	line = filereport:read()  
+	if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end
+	result =string.find(line,"result=\"success\"",1,true)
+	if result == nil then 
+		print("ERROR: incorrect result")
+	end
+	result =string.find(line,"timestamp=\"100\"",1,true)
+	if result == nil then 
+		print("ERROR: incorrect timestamp, get " .. line)
+	end
+	line = filereport:read()
+	if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end  
+	result =string.find(line,"<EXPECT_HMIEVENT FunctionName=\"Connected websocket\"/>",1,true)
+	if result == nil then 
+		print("ERROR: incorrect parameter line, get "..line)
+	end
+
+	line = filereport:read()
+	if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end  
+	result =string.find(line,"<hmi_connection FunctionName=\"SendRequest\">{[\"methodName\"] = \"New method\"}</hmi_connection>",1,true)
+	if result == nil then 
+		print("ERROR: incorrect parameter line with arguments, get "..line)
+	end
+
+	line = filereport:read()
+	if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end  
+	result =string.find(line,"</InitHMI>",1,true)
+	if result == nil then 
+		print("ERROR: expected close bracket of test case")
+	end
+
+	line = filereport:read()
+	if line == nil then 
+		print ("ERROR: unexpected end of xml file") 
+		return 
+	end  
+	if line ~= "</test_validationTest_lua>" then 
+		print("ERROR: expected close bracket of main test part")		
+	end
+   
+    print ("Analyze finished")
+
+	filereport:close()
+end
+
+
+--=================================
+print ("Report test started")
+print("==============\n")
+
+dates = os.date("%Y%m%d%H%M")
+
+xmlname = "validationTest.lua"
+createXmlFile(xmlname)
+
+ReportPath = FindDirectory("./Testing Reports/",dates)
+
+ReportPath = "Testing Reports/".. ReportPath.."/test"
+
+FindReportPath(ReportPath)
+
+xmlname = "validationTest"
+ReportName = FindReport(ReportPath,xmlname,dates)
+if ReportName ~= "" then 
+	AnalyzeXmlReport(ReportPath,ReportName)
+end
+print("============== \n")
+print ("Test ended")
+quit()
