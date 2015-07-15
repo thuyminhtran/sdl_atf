ba477e6d1a4d8ffefdf9d893a118bfe741b1183f
Alexander Kutsan <AKutsan@luxoft.com>
2015-07-15 16:14:17 +0300

    Rename LoggerMeta to Logger

--- modules/atf_logger.lua
+++ modules/atf_logger.lua
@@ -1,32 +1,32 @@
 local json = require("json")
-local LoggerMeta = {}
+local Logger = {}
 function time()
 	return os.time()
 end
 
-function LoggerMeta:MOBtoSDL(message)
+function Logger:MOBtoSDL(message)
 	log_str = string.format("MOB->SDL(%s) [version: %s, frameType: %s, encryption: %s, serviceType: %s, frameInfo: %s, messageID: %s] : %s ", time(), message["varsion"], message["frameType"], message["encryption"], message["serviceType"], message["frameInfo"], message["messageID"], message["payload"])
 	self.atf_log_file:write(log_str)
 end
 
-function LoggerMeta:SDLtoMOB(message)
+function Logger:SDLtoMOB(message)
 	log_str = string.format("MOB->SDL(%s) [version: %s, frameType: %s, encryption: %s, serviceType: %s, frameInfo: %s, messageID: %s] : %s ", time(), message["varsion"], message["frameType"], message["encryption"], message["serviceType"], message["frameInfo"], message["messageID"], message["payload"])
 	self.atf_log_file:write(log_str)
 end
 
-function LoggerMeta:HMItoMOB(mesage)
+function Logger:HMItoMOB(mesage)
 	log_str = string.format("MOB->SDL(%s) [version: %s, frameType: %s, encryption: %s, serviceType: %s, frameInfo: %s, messageID: %s] : %s ", time(), message["varsion"], message["frameType"], message["encryption"], message["serviceType"], message["frameInfo"], message["messageID"], message["payload"])
 	self.atf_log_file:write(log_str)
 end
 
-function LoggerMeta:SDLtoHMI(message)
+function Logger:SDLtoHMI(message)
 	log_str = string.format("MOB->SDL(%s) [version: %s, frameType: %s, encryption: %s, serviceType: %s, frameInfo: %s, messageID: %s] : %s ", time(), message["varsion"], message["frameType"], message["encryption"], message["serviceType"], message["frameInfo"], message["messageID"], message["payload"])
 	self.atf_log_file:write(log_str)
 end
 
 
-function LoggerMeta:New(path_to_log_file)
-	local logger = { mt = { __index = LoggerMeta}}
+function Logger:New(path_to_log_file)
+	local logger = { mt = { __index = Logger}}
 	logger.atf_log_file = io.open(path_to_log_file, "w+")
 	setmetatable(logger, logger.mt)
 	return logger
