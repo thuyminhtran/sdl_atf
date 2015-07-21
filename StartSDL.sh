0bccadb0f3c76c75373cf7f98508afd68c334b38
Alexander Kutsan <AKutsan@luxoft.com>
2015-07-21 15:05:50 +0300

    APPLINK-13552 Modify StopSDL to stop SDL correclty
    Stop SDL by SIGINT, if SDL won't stop during 10 secconds, kill it.
    Start sdl not in xterm to support build server

--- StartSDL.sh
+++ StartSDL.sh
@@ -5,10 +5,10 @@ dirATF=$(pwd)
 appName=$2
 cd $dirSDL
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. export LD_LIBRARY_PATH
-xterm -e ./$appName &
-cd $dirATF 
-touch sdl.pid
-echo $! > sdl.pid
+./$appName > /dev/null &
+sdl_pid=$!
+cd $dirATF
+echo $sdl_pid > sdl.pid
 sleep 3
 test -e sdl.pid && test -e /proc/$(cat sdl.pid) && exit 0
 exit 1
