7b24e4078b060f62d473dc439fb6bb291940c976
Alexander Kutsan <AKutsan@luxoft.com>
2015-10-06 12:27:28 +0300

    Fixed frameinfo overflow in consecutive frames
    Size of frame info is 1 byte
    If numer of consecutive frames was more than 255, it was setes to 0
    But 0 frameinfo is final framy by protocol.
    
    By reqirements(protocol v9.0):
    0x00 – Reserved for the last frame.
    Sequence Number will be incremented for each consecutive packet.
    It will roll over to 0x01 after reaching 0xFF.
    Last Frame shall have the sequence number of 0x00
    
    Related issue : APPLINK-13673

--- modules/protocol_handler/protocol_handler.lua
+++ modules/protocol_handler/protocol_handler.lua
@@ -164,7 +164,7 @@ function mt.__index:Compose(message)
       if frame_number == #multiframe_payloads then --last frame
         frame_info = 0
       else
-        frame_info = bit32.band(frame_number + 1, 0xFF)
+        frame_info = ((frame_number - 1) % 255) + 1
       end
       header = create_ford_header(message.version, message.encryption, kConsecutiveframe_frameType, message.serviceType,
         frame_info, message.sessionId, multiframe_payloads[frame_number], message.messageId)
