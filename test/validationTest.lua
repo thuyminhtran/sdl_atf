bf872166e33046b0528f0014d9c2dadf4a9bbf14
Elisey Zamakhov <EZamakhov@luxoft.com>
2016-02-18 21:47:03 +0200

    Update Validation test Fixed testing source code and success file Issue: APPLINK-21596

--- test/validationTest.lua
+++ test/validationTest.lua
@@ -1,6 +1,6 @@
 local validator = require("schema_validation")
 
-local json_hmi_tbl = { numTicks = 7,position = 6,sliderHeader ="sliderHeader",
+local json_hmi_tbl = { numTicks = 7, position = 6, sliderHeader ="sliderHeader",
   sliderFooter =
   {
     "sliderFooter1",
@@ -12,11 +12,12 @@ local json_hmi_tbl = { numTicks = 7,position = 6,sliderHeader ="sliderHeader",
     "sliderFooter7",
   },
   timeout = 3000,
+  appID = 1
 }
 
 local json_mob_tbl = {success = true, resultCode = {"SUCCESS"}}
 
-local _res, _err = validator.validate_hmi_request('Slider', json_hmi_tbl)
+local _res, _err = validator.validate_hmi_request('UI.Slider', json_hmi_tbl)
 if (not _res) then print("validate_hmi_request:"..tostring(_res).." ==> ".._err) else print ("validate_hmi_request:"..tostring(_res)) end
 
 _res, _err = validator.validate_mobile_response('Slider',json_mob_tbl,true)
@@ -25,10 +26,10 @@ if (not _res) then print("validate_mobile_response:"..tostring(_res).." ==> ".._
 _res, _err = validator.validate_mobile_response( 'Slider',{ success = 'true', resultCode = {"SUCCESS"} },true)
 if (not _res) then print("validate_mobile_response:"..tostring(_res).." ==> ".._err) else print ("validate_mobile_response:"..tostring(_res)) end
 
-_res, _err = validator.validate_hmi_notification( 'OnSystemRequest', { requestType = {"PROPRIETARY"}, urlSchema = "default"})
+_res, _err = validator.validate_hmi_notification( 'BasicCommunication.OnSystemRequest', { requestType = {"PROPRIETARY"}})
 if (not _res) then print("validate_hmi_notification:"..tostring(_res).." ==> ".._err) else print ("validate_hmi_notification:"..tostring(_res)) end
 
-_res, _err = validator.validate_hmi_notification( 'OnSystemRequest', { urlSchema = "default"},true)
+_res, _err = validator.validate_hmi_notification( 'BasicCommunication.OnSystemRequest', { urlSchema = "default", fileName = "fileName"}, true)
 if (not _res) then print("validate_hmi_notification:"..tostring(_res).." ==> ".._err) else print ("validate_hmi_notification:"..tostring(_res)) end
 
 _res, _err = validator.validate_mobile_notification( "OnHMIStatus", {hmiLevel = "FULL"})
@@ -40,7 +41,7 @@ if (not _res) then print("json_validate:"..tostring(_res).." ==> ".._err) else p
 _res, _err = validator.json_validate(json_mob_tbl, { success = 'true', resultCode = {"SUCCESS"} } )
 if (not _res) then print("json_validate:"..tostring(_res).." ==> ".._err) else print ("json_validate:"..tostring(_res)) end
 
-local _res, _err = validator.validate_hmi_request('PerformInteraction',
+local _res, _err = validator.validate_mobile_request('PerformInteraction',
   { initialText = "initialText",
     initialPrompt = {{text = "initialPrompt", type = "TEXT"}},
     interactionMode = "MANUAL_ONLY",
