041bb2032df85c5e5ede3ea8bff05c9708f64632
Herasym Oleh <oolleehh@gmail.com>
2016-03-07 14:37:33 +0200

    Add logs to StartSDL & StopSDL methods
    Related:
            APPLINK-14138

--- modules/SDL.lua
+++ modules/SDL.lua
@@ -16,14 +16,18 @@ function SDL:StartSDL(pathToSDL, smartDeviceLinkCore, ExitOnCrash)
   if status == self.STOPPED then
     local result = os.execute ('./StartSDL.sh ' .. pathToSDL .. ' ' .. smartDeviceLinkCore)
     if result then
+      local msg = "SDL started"
+      xmlReporter.AddMessage("StartSDL", {["message"] = msg})
       return true
     else
       local msg = "SDL had already started not from ATF or unexpectedly crashed"
+      xmlReporter.AddMessage("StartSDL", {["message"] = msg})
       print(console.setattr(msg, "cyan", 1))
       return nil, msg
     end
   end
   local msg = "SDL had already started from ATF"
+  xmlReporter.AddMessage("StartSDL", {["message"] = msg})
   print(console.setattr(msg, "cyan", 1))
   return nil, msg
 end
@@ -39,6 +43,7 @@ function SDL:StopSDL()
     end
   else
     local msg = "SDL had already stopped"
+    xmlReporter.AddMessage("StopSDL", {["message"] = msg})
     print(console.setattr(msg, "cyan", 1))
     return nil, msg
   end
