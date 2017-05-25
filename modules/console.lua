4e426ebc84a75b5c1776985f2ce6df614a7b0e36
Alexandr Deriabin <aderiabin@luxoft.com>
2017-05-25 18:36:34 +0300

    Update console with LDoc documentation

--- modules/console.lua
+++ modules/console.lua
@@ -1,6 +1,20 @@
-local module = { }
+--- Module that provides functions for working with the console
+--
+-- *Dependencies:* none
+--
+-- *Globals:* `c`, `b`, `u`, `suffix`
+-- @copyright [Ford Motor Company](https://smartdevicelink.com/partners/ford/) and [SmartDeviceLink Consortium](https://smartdevicelink.com/consortium/)
+-- @license <https://github.com/smartdevicelink/sdl_core/blob/master/LICENSE>
 
-function module.setattr(string, color, bold, underline)
+local console = { }
+
+--- Build attribute that represents string with applied style
+-- @tparam string string Attribute string
+-- @tparam string color Attribute style modifier color
+-- @tparam string bold Attribute style modifier bold
+-- @tparam string underline Attribute style modifier underline
+-- @treturn string Attribute with applied style
+function console.setattr(string, color, bold, underline)
   if color == "black" then c = '30'
   elseif color == "red" then c = '31'
   elseif color == "green" then c = '32'
@@ -36,4 +50,4 @@ function module.setattr(string, color, bold, underline)
   return prefix .. string .. suffix
 end
 
-return module
+return console
