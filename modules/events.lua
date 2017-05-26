eb98d7633e9cb16bec2427ac6044da1df3bcfb9b
Alexandr Deriabin <aderiabin@luxoft.com>
2017-05-26 10:26:37 +0300

    Update events with LDoc documentation

--- modules/events.lua
+++ modules/events.lua
@@ -1,18 +1,43 @@
-local module = {}
-module.connectedEvent = { level = 3 }
-module.disconnectedEvent = { level = 3 }
-module.timeoutEvent = { level = 3 }
+--- Module which is responsible for events with expectations handling
+--
+-- *Dependencies:* none
+--
+-- *Globals:* none
+-- @copyright [Ford Motor Company](https://smartdevicelink.com/partners/ford/) and [SmartDeviceLink Consortium](https://smartdevicelink.com/consortium/)
+-- @license <https://github.com/smartdevicelink/sdl_core/blob/master/LICENSE>
+
+--- Singleton table which provide predefined events and interface for construction new events.
+-- @table Events
+-- @tfield Event connectedEvent Predefined connected event
+-- @tfield Event disconnectedEvent Predefined disconnected event
+-- @tfield Event timeoutEvent Predefined timeout event
+local Events = {}
 local event_mt = { __index = { } }
-setmetatable(module.connectedEvent, event_mt)
-setmetatable(module.disconnectedEvent, event_mt)
-setmetatable(module.timeoutEvent, event_mt)
+---
+Events.connectedEvent = { level = 3 }
+Events.disconnectedEvent = { level = 3 }
+Events.timeoutEvent = { level = 3 }
 
-function event_mt.__index:matches() return false end
+setmetatable(Events.connectedEvent, event_mt)
+setmetatable(Events.disconnectedEvent, event_mt)
+setmetatable(Events.timeoutEvent, event_mt)
 
-function module.Event()
-  local ret = { level = 3 }
+--- Type which represents event in ATF event-expectation system
+-- @type Event
+
+--- Construct instance of Event type
+-- @treturn Event Constructed instance
+function Events.Event()
+  local ret = {
+    --- Level of event
+    level = 3
+  }
   setmetatable(ret, event_mt)
   return ret
 end
 
-return module
+--- Event comparation function
+-- @treturn boolean True if events are equal
+function event_mt.__index:matches() return false end
+
+return Events
