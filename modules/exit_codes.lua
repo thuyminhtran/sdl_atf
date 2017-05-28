b4ce2f16466087cd2440403ef0a934fe6bac2971
Alexandr Deriabin <aderiabin@luxoft.com>
2017-05-28 11:40:00 +0300

    Update exit_codes with LDoc documentation

--- modules/exit_codes.lua
+++ modules/exit_codes.lua
@@ -1,7 +1,21 @@
+--- Module which represents enum of exit codes for ATF
+--
+-- *Dependencies:* none
+--
+-- *Globals:* none
+-- @module exit_codes
+-- @copyright [Ford Motor Company](https://smartdevicelink.com/partners/ford/) and [SmartDeviceLink Consortium](https://smartdevicelink.com/consortium/)
+-- @license <https://github.com/smartdevicelink/sdl_core/blob/master/LICENSE>
+
 local exit_codes ={}
+
+--- Test execution was finished with result SUCCESS
 exit_codes.success = 0
+--- Test execution was finished with result ABORTED (test script issue)
 exit_codes.aborted = 1
+--- Test execution was finished with result FAILED (SDL issue)
 exit_codes.failed = 2
+--- Test execution was finished with result WRONG ARGUMENTS (test run issue)
 exit_codes.wrong_arg = 3
 
 return exit_codes
