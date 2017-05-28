0c2b9a7d182425cbdeb70913bb42eb61c3eb0a9e
Alexandr Deriabin <aderiabin@luxoft.com>
2017-05-28 12:48:48 +0300

    Update function_id with LDoc documentation

--- modules/function_id.lua
+++ modules/function_id.lua
@@ -1,7 +1,20 @@
-local xml = require('xml')
-local module = { }
+--- Module which is responsible for creation table with mobile functions from Mobile_API.xml
+--
+-- *Dependencies:* `xml`, `config`
+--
+-- *Globals:* `console`
+-- @module function_id
+-- @copyright [Ford Motor Company](https://smartdevicelink.com/partners/ford/) and [SmartDeviceLink Consortium](https://smartdevicelink.com/consortium/)
+-- @license <https://github.com/smartdevicelink/sdl_core/blob/master/LICENSE>
 
-module.mobile_functions = { }
+local xml = require('xml')
+local FunctionId = { }
+
+--- Table with mobile functions.
+--
+-- Record: <function_name> : <function_id>
+-- @table mobile_functions
+FunctionId.mobile_functions = { }
 
 local mobile_api = xml.open("data/MOBILE_API.xml")
 
@@ -12,7 +25,7 @@ for _, f in ipairs(mobile_api:xpath("//enum[@name='FunctionID']/element")) do
 end
 
 for _, f in ipairs(mobile_api:xpath("//function")) do
-  module.mobile_functions[f:attr("name")] = tonumber(ids[f:attr("functionID")])
+  FunctionId.mobile_functions[f:attr("name")] = tonumber(ids[f:attr("functionID")])
 end
 
-return module.mobile_functions
+return FunctionId.mobile_functions
