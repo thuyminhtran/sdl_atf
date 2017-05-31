0ee0320a2fda39b08dba92ce814c3ad007768062
Alexandr Deriabin <aderiabin@luxoft.com>
2017-05-31 16:00:36 +0300

    Update testbase with LDoc documentation

--- modules/testbase.lua
+++ modules/testbase.lua
@@ -1,13 +1,18 @@
----- Test Cases executor.
+---- Base test script executor.
 --
---  Runs all new methods with a first Capital letter as a Tests.
---  Tests are being executed one by one and interrupt execution is case of
---  any critical issues (each Test could be marked as Critical)
+-- Runs all new methods with a first Capital letter as a test steps.
+-- Test steps are being executed one by one and interrupt execution is case of
+-- any critical issues (each test step could be marked as Critical)
 --
---  For component overview description and a list of responsibilities, please, follow [ATF SAD Component View](https://smartdevicelink.com/en/guides/pull_request/93dee199f30303b4b26ec9a852c1f5261ff0735d/atf/components-view/#test-base).
---  @module TestBase
---  @copyright [Ford Motor Company](https://smartdevicelink.com/partners/ford/) and [SmartDeviceLink Consortium](https://smartdevicelink.com/consortium/)
---  @license <https://github.com/smartdevicelink/sdl_core/blob/master/LICENSE>
+-- For component overview description and a list of responsibilities, please, follow [ATF SAD Component View](https://smartdevicelink.com/en/guides/pull_request/93dee199f30303b4b26ec9a852c1f5261ff0735d/atf/components-view/#test-base).
+--
+-- *Dependencies:* `qt`, `event_dispatcher`, `events`, `expectations`, `console`, `format`, `SDL`, `exit_codes`, `config`
+--
+-- *Globals:* `xmlReporter`, `qt`, `critical()`, `description()`, `timestamp()`, `atf_logger`, `print_stopscript()`,
+-- `is_redirected`, `config`, `event_dispatcher`, `quit`, `timeoutTimer`
+-- @module testbase
+-- @copyright [Ford Motor Company](https://smartdevicelink.com/partners/ford/) and [SmartDeviceLink Consortium](https://smartdevicelink.com/consortium/)
+-- @license <https://github.com/smartdevicelink/sdl_core/blob/master/LICENSE>
 
 local ed = require("event_dispatcher")
 local events = require("events")
@@ -17,7 +22,7 @@ local fmt = require('format')
 local SDL = require('SDL')
 local exit_codes = require('exit_codes')
 
-local module = { }
+local Test = { }
 
 local Expectation = expectations.Expectation
 local SUCCESS = expectations.SUCCESS
@@ -29,30 +34,50 @@ local CRASH = SDL.CRASH
 
 local total_testset_result = true
 
--- Support table for controlling Test Cases execution status
--- @table control
+--- Support table for controlling test steps execution status
 local control = qt.dynamic()
 
+--- Check whether character is capital
+-- @tparam string c Character for check
+-- @treturn boolean True in case parameter is capital letter
 local function isCapital(c)
   return 'A' <= c and c <= 'Z'
 end
 
 os.setlocale("C")
 
+--- Module Test members
+-- @section Test
+
 local mt =
 {
   __index =
   {
+    --- List of test steps
     test_cases = { },
+    --- List of test step names
     case_names = { },
+    --- List of test step descriptions
     descriptions = { },
+    --- Current test step name
     current_case_name = nil,
+    --- Current test step index in lists
     current_case_index = 0,
+    --- Flag which defines whether current test step is critical
     current_case_mandatory = false,
+    --- List of test step expectations
     expectations_list = expectations.ExpectationsList(),
+    --- Add expectation to list of expectations
+    -- @function AddExpectation
+    -- @tparam table self Test
+    -- @tparam Expectation e Expectation to be added
     AddExpectation = function(self,e)
       self.expectations_list:Add(e)
     end,
+    --- Remove expectation from list of expectations
+    -- @function RemoveExpectation
+    -- @tparam table self Test
+    -- @tparam Expectation e Expectation to be removed
     RemoveExpectation = function(self, e)
       self.expectations_list:Remove(e)
     end,
@@ -61,13 +86,17 @@ local mt =
     local firstLetter = string.sub(k, 1, 1)
     if type(v) == "function" and isCapital(firstLetter)then
       local function testcase(test)
+        --- Global functions
+        -- @section Global
+
+        --- Set current test step description
+        --  @tparam string desc Description
         function description(desc)
           t.descriptions[k] = desc
         end
-        --- Set current test criticalness
-        --  Failed critical test stops all following tests execution
-        --  @param is_critical new bool value of current test criticalness
-        --  @function TestBase:critical
+        --- Set current test step criticalness
+        --  Failed critical test step stops all following tests execution
+        --  @tparam boolean val New bool value of current test criticalness
         function critical(val)
           t.current_case_mandatory = val
         end
@@ -85,26 +114,25 @@ local mt =
   __metatable = { }
 }
 
-
 --- Runs next Test Case or quit ATF execution
---  Test case is any testbase inheritor
+-- Test case is any testbase inheritor
 -- with a first capital letter
 -- @lfunction control.runNextCase
 function control.runNextCase()
-  module.ts = timestamp()
-  module.current_case_time = atf_logger.formated_time(true)
-  module.current_case_index = module.current_case_index + 1
-  local testcase = module.test_cases[module.current_case_index]
+  Test.ts = timestamp()
+  Test.current_case_time = atf_logger.formated_time(true)
+  Test.current_case_index = Test.current_case_index + 1
+  local testcase = Test.test_cases[Test.current_case_index]
   if testcase then
-    module.current_case_name = module.case_names[testcase]
-    xmlReporter.AddCase(module.current_case_name)
-    atf_logger.LOGTestCaseStart(module.current_case_name)
-    testcase(module)
+    Test.current_case_name = Test.case_names[testcase]
+    xmlReporter.AddCase(Test.current_case_name)
+    atf_logger.LOGTestCaseStart(Test.current_case_name)
+    testcase(Test)
   else
     if SDL.autoStarted then
       SDL:StopSDL()
     end
-    module.current_case_name = nil
+    Test.current_case_name = nil
     print_stopscript()
     xmlReporter:finalize()
     if total_testset_result == false then
@@ -128,7 +156,7 @@ end
 --- Checks Test Case result and SDL status
 --- In case of any critical issues - interrupts Test Suit execution
 local function CheckStatus()
-  if module.current_case_name == nil or module.current_case_name == '' then return end
+  if Test.current_case_name == nil or Test.current_case_name == '' then return end
   -- Check the test status
   local success = true
   local errorMessage = {}
@@ -139,8 +167,8 @@ local function CheckStatus()
     print(console.setattr("SDL has unexpectedly crashed or stop responding!", "cyan", 1))
     critical(SDL.exitOnCrash)
     SDL:DeleteFile()
-  elseif module.expectations_list:Any(function(e) return not e.status end) then return end
-  for _, e in ipairs(module.expectations_list) do
+  elseif Test.expectations_list:Any(function(e) return not e.status end) then return end
+  for _, e in ipairs(Test.expectations_list) do
     if e.status ~= SUCCESS then
       success = false
       total_testset_result = false
@@ -152,12 +180,12 @@ local function CheckStatus()
       errorMessage[e.name .. ": " .. k] = v
     end
   end
-  fmt.PrintCaseResult(module.current_case_time, module.current_case_name, success, errorMessage, timestamp() - module.ts)
-  xmlReporter.CaseMessageTotal(module.current_case_name,{ ["result"] = success, ["timestamp"] = (timestamp() - module.ts)} )
+  fmt.PrintCaseResult(Test.current_case_time, Test.current_case_name, success, errorMessage, timestamp() - Test.ts)
+  xmlReporter.CaseMessageTotal(Test.current_case_name,{ ["result"] = success, ["timestamp"] = (timestamp() - Test.ts)} )
   if (not success) then xmlReporter.AddMessage("ErrorMessage", {["Status"] = "FAILD"}, errorMessage ) end
-  module.expectations_list:Clear()
-  module.current_case_name = nil
-  if module.current_case_mandatory and not success then
+  Test.expectations_list:Clear()
+  Test.current_case_name = nil
+  if Test.current_case_mandatory and not success then
     SDL:StopSDL()
     if SDL.exitOnCrash == true then
       quit(exit_codes.aborted)
@@ -169,12 +197,12 @@ end
 --- Fail the current Test execution
 -- @param self TestBase table
 -- @param cause reason of test fail
--- @function FailTestCase
+-- @lfunction FailTestCase
 local function FailTestCase(self, cause)
   local exp = expectations.Expectation("AutoFail")
   exp.status = FAILED
   table.insert(exp.errorMessage, cause)
-  module.expectations_list:Add(exp)
+  Test.expectations_list:Add(exp)
   CheckStatus()
 end
 
@@ -185,13 +213,13 @@ function control:checkstatus()
   CheckStatus()
 end
 
---- testbase module initialization on `testbase.lua` loading
+--- Testbase module initialization
 local function main()
-  setmetatable(module, mt)
+  setmetatable(Test, mt)
 
   qt.connect(control, "next()", control, "runNextCase()")
 
-  rawset(module, "FailTestCase", FailTestCase)
+  rawset(Test, "FailTestCase", FailTestCase)
 
   event_dispatcher = ed.EventDispatcher()
   event_dispatcher:OnPostEvent(CheckStatus)
@@ -204,4 +232,5 @@ end
 
 -- Execute main and return result metatable
 main()
-return module
+
+return Test
